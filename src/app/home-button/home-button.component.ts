37e6f00559e71660a21516f60b63bf28cafe44d5
Renan <gohautrenan@gmail.com>
2024-09-23 15:58:41 +0200

    add home btn logique

--- src/app/home-button/home-button.component.ts
+++ src/app/home-button/home-button.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserService } from '../browser.service'; 
 
 @Component({
   selector: 'app-home-button',
   standalone: true,
   imports: [MatIconModule, MatButtonModule],
   templateUrl: './home-button.component.html',
-  styleUrl: './home-button.component.css'
+  styleUrls: ['./home-button.component.css']
 })
 export class HomeButtonComponent {
 
+  constructor(private browserService: BrowserService) {}
+
+  goHome() {
+    this.browserService.goToPage("https://amiens.unilasalle.fr");
+  }
 }
